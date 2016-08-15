7b7deea6fe6f49d58d71460dd7d0fd89894dfb46
Jesus Manuel Garnica Bonome <j.garnica@lab.matcom.uh.cu>
2016-08-15 03:15:35 -0700

    ast

---
+++ TigerCs/Generation/Semantic/AST/ExpresionNode.cs
@@ -0,0 +1,14 @@
+﻿using TigerCs.Generation.ByteCode;
+using TigerCs.Generation.Semantic.Scopes;
+
+namespace TigerCs.Generation.Semantic.AST
+{
+	public abstract class ExpresionNode
+	{
+		public abstract bool CheckSemantics(TigerScope scope, ErrorReport report);
+
+		public abstract void GenerateCode(ITigerEmitter cg);
+
+		public abstract TypeInfo Return { get; }
+	}
+}
