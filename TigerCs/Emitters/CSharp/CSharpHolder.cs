cb0d5ef4554bce616acb3b39dafb14ea940b8514
Jesus Manuel Garnica Bonome <j.garnica@lab.matcom.uh.cu>
2016-08-16 14:11:33 -0700

    forget to track this

---
+++ TigerCs/Emitters/CSharp/CSharpHolder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TigerCs.Generation.ByteCode;
+
+namespace TigerCs.Emitters.CSharp
+{
+	public class CSharpHolder : IHolder
+	{
+		public IType Type { get; set; }
+
+		public CSharpHolder Nested { get; set; }
+
+		public string access { get; set; }
+
+		public bool Assignable
+		{
+			get; set;
+		}
+	}
+}
