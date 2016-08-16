3ecfda77a51d01f7c21628e869a99eff458e6a60
Jesus Manuel Garnica Bonome <j.garnica@lab.matcom.uh.cu>
2016-08-15 18:08:22 -0700

    scopes definition

--- TigerCs/Generation/Semantic/Scopes/TigerScope.cs
+++ TigerCs/Generation/Semantic/Scopes/TigerScope.cs
@@ -8,10 +8,31 @@ namespace TigerCs.Generation.Semantic.Scopes
 {
 	public class TigerScope
 	{
-		public TigerScope Parent { get; protected set; }
-		public List<TigerScope> Children { get; protected set; }
-		protected Dictionary<string, MemberInfo> Namespace { get; set; }
+		public TigerScope Parent { get; protected set; } = null;
+		public List<TigerScope> Children { get; protected set; } = new List<TigerScope>();
+		protected Dictionary<string, MemberInfo> Namespace { get; set; } = new Dictionary<string, MemberInfo>();
+		public bool ContainsTypeDefinitions { get; protected set; } = false;
 
+		public bool DeclareMember(string name, MemberInfo member)
+		{
+			MemberInfo existent;
+			if (Reachable(name, out existent)) return false;
 
+			Namespace[name] = member;
+			if (member is TypeInfo) ContainsTypeDefinitions = true;
+			return true;
+		}
+
+		public bool Reachable(string name, out MemberInfo member)
+		{
+			var current = this;
+			do
+			{
+				if (current.Namespace.TryGetValue(name, out member)) return true;
+				current = current.Parent;
+			}
+			while (current != null);
+			return false;
+		}
 	}
 }
