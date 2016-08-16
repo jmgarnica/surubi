f9bfabe2507258c64f1c0c4dea7c614a52d98f1b
Jesus Manuel Garnica Bonome <j.garnica@lab.matcom.uh.cu>
2016-08-15 18:11:09 -0700

    CSharp Emitter implementation of a vm

---
+++ TigerCs/Emitters/CSharp/CSharpFunction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TigerCs.Generation.ByteCode;
+
+namespace TigerCs.Emitters.CSharp
+{
+	public class CSharpFunction : IFunction
+	{
+		public CSharpFunction(string access, IType Return,bool bounded = true)
+		{
+			this.access = access;
+			this.Return = Return;
+			Bounded = bounded;
+		}
+
+		public CSharpFunction(IType Return)
+		{
+			this.Return = Return;
+			Bounded = false;
+		}
+
+		public bool Bounded
+		{
+			get; set;
+		}
+
+		public string access { get; set; }
+
+		public IType Return
+		{
+			get; set;
+		}
+	}
+}
---
+++ TigerCs/Emitters/CSharp/CSharpType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TigerCs.Generation.ByteCode;
+
+namespace TigerCs.Emitters.CSharp
+{
+	public class CSharpType : IType
+	{
+		public CSharpType(string dotnetname, bool array = false)
+		{
+			DotNetName = dotnetname;
+			Array = array;
+		}
+		public bool Array
+		{
+			get; set;
+		}
+
+		public string DotNetName { get; set; }
+
+		public bool Equal(IType ty)
+		{
+			if (ty is CSharpType)
+			{
+				return ((CSharpType)ty).DotNetName == DotNetName;
+			}
+			return false;
+		}
+	}
+}
