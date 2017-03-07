865a899caa93249079ec97157a17615df6951b12
Carlos David Muñiz Chall <c.chall@lab.matcom.uh.cu>
2017-03-07 12:49:17 -0800

    starting grammar

---
+++ TigerCs/Parser/IParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TigerCs.Generation.AST.Expressions;
+using System.IO;
+namespace TigerCs.Parser
+{
+    public interface IParser
+    {
+        IExpression Parse(TextReader tr);
+    }
+}
---
+++ TigerCs/Parser/Parser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TigerCs.Generation.AST.Expressions;
+
+namespace TigerCs.Parser
+{
+    class Parser : IParser
+    {
+        public IExpression Parse(TextReader tr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
